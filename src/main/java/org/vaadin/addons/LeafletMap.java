347e8a86b89e4a41196b4bc53c1de8031c416812
Przemyslaw Sporysz <p.sporysz@gmail.com>
2018-12-07 15:46:52 +0100

    Add-on Component Starter for Flow

---
+++ src/main/java/org/vaadin/addons/LeafletMap.java
@@ -0,0 +1,13 @@
+package org.vaadin.addons;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+
+@Tag("leaflet-map")
+@HtmlImport("bower_components/leaflet-map/leaflet-map.html")
+public class LeafletMap extends Component {
+
+    public LeafletMap() {
+    }
+}
